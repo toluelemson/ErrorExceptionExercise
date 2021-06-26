4a3e641f89758fa1ece9dd12d5e116166c8c5494
Toluwani <toluwhani@gmail.com>
2021-06-27 02:10:36 +0300

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new D();
+            }
+
+            catch (B obj)
+            {
+                Console.WriteLine(obj);
+            };
+        }
+    }
+
+    class B : Exception
+    {}
+
+    class D : B
+    {}
+}
+
+
